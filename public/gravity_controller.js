af4b79d15712db49659d7d4f59cc96eb23f40d80
mkolkowski <marius.kolkowski@esemos.de>
2016-03-03 21:33:37 +0100

    - refactor code

--- public/gravity_controller.js
+++ public/gravity_controller.js
@@ -1,11 +1,13 @@
 define(function (require) {
+  var getSort = require('ui/doc_table/lib/get_sort');
 
   require('ui/notify');
 
   var module = require('ui/modules').get('kibana/kibana-gravity', ['kibana']);
-  module.controller('KbnGravityVisController', function ($scope, $sce, $route, config, Private, Notifier, AppState) {
+  module.controller('KbnGravityVisController', function ($scope, $sce, $route, config, Private, Notifier) {
+    $scope.hits = 0;
     $scope.gravities = [];
-    console.log("kibana-gravity:$scope");
+    $scope.route = $route;
 
     var elasticHitToGravity = function(hit) {
       var gravity = {
@@ -20,41 +22,29 @@ define(function (require) {
       return gravity;
     };
 
-    $scope.test = $route;
-
     var HitSortFn = Private(require('plugins/kibana/discover/_hit_sort_fn'));
     var notify = new Notifier({
-      location: 'Discover'
+      location: 'Dashboard'
     });
-    //var $state = $scope.state = new AppState(getStateDefaults());
 
     $scope.searchSource = $route.current.locals.dash.searchSource;
     $scope.indexPattern = $scope.vis.indexPattern;
     $scope.searchSource.set('index', $scope.indexPattern);
-    $scope.size = 10;
     $scope.opts = {
-      // number of records to fetch, then paginate through
-      sampleSize: config.get('discover:sampleSize'),
-      // Index to match
+      query: $scope.searchSource.get('query') || '',
+      sort: getSort.array(["time", "desc"], $scope.indexPattern),
       index: $scope.indexPattern.id,
-      timefield: $scope.indexPattern.timeFieldName
+      filters: _.cloneDeep($scope.searchSource.getOwn('filter')),
+      size: 10
     };
 
     $scope.searchSource.onBeginSegmentedFetch(function (segmented) {
-
       function flushResponseData() {
         $scope.hits = 0;
-        $scope.faliures = [];
-        $scope.rows = [];
-        $scope.fieldCounts = {};
+        $scope.gravities = [];
       }
 
-      if (!$scope.rows) flushResponseData();
-
-      var sort = ["time"];
-      //var sort = $state.sort;
-      var timeField = $scope.indexPattern.timeFieldName;
-      var totalSize = $scope.size || $scope.opts.sampleSize;
+      if (!$scope.gravities) flushResponseData();
 
       /**
        * Basically an emum.
@@ -67,21 +57,21 @@ define(function (require) {
        * @type {String}
        */
       var sortBy = (function () {
-        if (!_.isArray(sort)) return 'implicit';
-        else if (sort[0] === '_score') return 'implicit';
-        else if (sort[0] === timeField) return 'time';
+        if (!_.isArray($scope.opts.sort)) return 'implicit';
+        else if ($scope.opts.sort[0] === '_score') return 'implicit';
+        else if ($scope.opts.sort[0] === $scope.indexPattern.timeFieldName) return 'time';
         else return 'non-time';
       }());
 
       var sortFn = null;
       if (sortBy !== 'implicit') {
-        sortFn = new HitSortFn(sort[1]);
+        sortFn = new HitSortFn($scope.opts.sort[1]);
       }
 
-      if (sort[0] === '_score') segmented.setMaxSegments(1);
-      segmented.setDirection(sortBy === 'time' ? (sort[1] || 'desc') : 'desc');
+      if ($scope.opts.sort[0] === '_score') segmented.setMaxSegments(1);
+      segmented.setDirection(sortBy === 'time' ? ($scope.opts.sort[1] || 'desc') : 'desc');
       segmented.setSortFn(sortFn);
-      segmented.setSize($scope.opts.sampleSize);
+      segmented.setSize($scope.opts.size);
 
       // triggered when the status updated
       segmented.on('status', function (status) {
@@ -106,10 +96,10 @@ define(function (require) {
         $scope.hits = merged.hits.total;
 
         // the merge rows, use a new array to help watchers
-        $scope.rows = merged.hits.hits.slice();
+        var rows = merged.hits.hits.slice();
         $scope.gravities = [];
-        for(var i = 0; i < $scope.rows.length; i++) {
-          var hit = $scope.rows[i];
+        for(var i = 0; i < rows.length; i++) {
+          var hit = rows[i];
           var gravity = elasticHitToGravity(hit);
           $scope.gravities.push(gravity);
         }
