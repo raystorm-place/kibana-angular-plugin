7851a0306253d0a3d75780bf2f2e7d4aa0310d88
mkolkowski <marius.kolkowski@esemos.de>
2016-06-03 00:24:43 +0200

    - cleanup code

--- public/angularDocument.js
+++ public/angularDocument.js
@@ -4,18 +4,11 @@ define(function (require) {
   require('plugins/kibana-angular-plugin/bower_components/ace-builds/src-min-noconflict/theme-monokai.js');
   require('plugins/kibana-angular-plugin/bower_components/angular-ui-ace/ui-ace.min.js');
   require('plugins/kibana-angular-plugin/angularDocument.less');
-
-  // we also need to load the controller and used by the template
   require('plugins/kibana-angular-plugin/angularDocumentController');
-
-  // register the provider with the visTypes registry so that other know it exists
   require('ui/registry/vis_types').register(AngularVisProvider);
 
   function AngularVisProvider(Private) {
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
-
-    // return the visType object, which kibana will use to display and configure new
-    // Vis object of this type.
     return new TemplateVisType({
       name: 'angularDocument',
       title: 'Angular Widget',
@@ -27,7 +20,5 @@ define(function (require) {
       }
     });
   }
-
-  // export the provider so that the visType can be required with Private()
   return AngularVisProvider;
 });
